72521ce25718fa59a3e63401ef38e53be40e7d74
terryphillips1990123 <sleepingdragon1990123@gmail.com>
2023-06-30 06:00:26 -0700

    final

---
+++ js/CLang.js
@@ -0,0 +1,22 @@
+TEXT_GAMEOVER_1 = "🤩Congratulations!🤩\nClick button to proceed.";
+TEXT_GAMEOVER_2 = "Try Again"
+TEXT_OF = "/";
+TEXT_SCORE = "SCORE";
+TEXT_BEST_SCORE = "BEST SCORE";
+TEXT_MULTIPLIER = "x";
+TEXT_ARE_SURE = "ARE YOU SURE?";
+TEXT_BALL_OUT = "OUT😢";
+TEXT_PAUSE = "PAUSE";
+TEXT_CONGRATULATION = ["NICE ONE!🥰", "GOOD!🕺", "BRILLIANT!!🤩"];
+TEXT_SAVED = "SAVED🙄";
+TEXT_HELP = "SWIPE TO KICK THE BALL";
+TEXT_DEVELOPED = "BROUGHT TO YOU BY";
+var TEXT_PRELOADER_CONTINUE = "START";
+
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "Congratulations!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better?";
